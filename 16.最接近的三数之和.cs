edeb760b334edf30b8687ee81989ca0dcd00738c
mimizao <88256191@qq.com>
2022-07-17 17:12:18 +0800

    three_sum_closest own idea rust

--- 16.最接近的三数之和.cs
+++ 16.最接近的三数之和.cs
@@ -15,6 +15,43 @@ public class Solution
     {
         Array.Sort(nums);
         int res = nums[0] + nums[1] + nums[2];
+        int difference = int.MaxValue;
+        for (int first = 0; first < nums.Length - 2; first++)
+        {
+            if (first > 0 && nums[first] == nums[first - 1])
+            {
+                first++;
+                continue;
+            }
+            int second = first + 1;
+            int third = nums.Length - 1;
+            while (third > second)
+            {
+                int newDifference = Math.Abs(nums[first] + nums[second] + nums[third] - target);
+                if (newDifference < difference)
+                {
+                    difference = newDifference;
+                    res = nums[first] + nums[second] + nums[third];
+                }
+                if (target > nums[first] + nums[second] + nums[third])
+                {
+                    second++;
+                    while (second < third && nums[second] == nums[second + 1])
+                    {
+                        second++;
+                    }
+                }
+                else
+                {
+                    third--;
+                    while (second < third && nums[third] == nums[third - 1])
+                    {
+                        third--;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
