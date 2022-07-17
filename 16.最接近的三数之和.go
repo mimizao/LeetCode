b1ba5002e3289200bf0bf75dfb742c85fc8b120f
mimizao <88256191@qq.com>
2022-07-17 15:30:35 +0800

    threeSumClosest own idea go

---
+++ 16.最接近的三数之和.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=golang
+ *
+ * [16] 最接近的三数之和
+ */
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+// @lc code=start
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	res := nums[0] + nums[1] + nums[2]
+	difference := int(math.Abs((float64)(res - target)))
+	for first := 0; first < len(nums)-2; first++ {
+		second := first + 1
+		third := len(nums) - 1
+		for second < third {
+			newDifference := int(math.Abs((float64)(nums[first] + nums[second] + nums[third] - target)))
+			if newDifference == 0 {
+				return target
+			}
+			if newDifference < difference {
+				difference = newDifference
+				res = nums[first] + nums[second] + nums[third]
+			}
+			if target > nums[first]+nums[second]+nums[third] {
+				second++
+			} else {
+				third--
+			}
+		}
+	}
+	return res
+}
+
+// @lc code=end
