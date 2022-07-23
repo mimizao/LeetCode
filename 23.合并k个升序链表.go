e4f304179f05a035e6c84db32df12b999ef40cb0
mimizao <88256191@qq.com>
2022-07-23 20:53:48 +0800

    mergeKLists own idea go

---
+++ 23.合并k个升序链表.go
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=golang
+ *
+ * [23] 合并K个升序链表
+ */
+
+package main
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	if len(lists) == 1 {
+		return lists[0]
+	}
+	var res *ListNode
+	var max = 10001
+	var index = 0
+	for i := 0; i < len(lists); i++ {
+		if lists[i] != nil && lists[i].Val < max {
+			index = i
+			max = lists[i].Val
+		}
+	}
+	res = lists[index]
+	if lists[index] != nil {
+		lists[index] = lists[index].Next
+		res.Next = mergeKLists(lists)
+	}
+	return res
+}
+
+// @lc code=end
+
+func mergeKLists1(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	res := lists[0]
+	for i := 1; i < len(lists); i++ {
+		res = newMergeTwoLists(res, lists[i])
+	}
+	return res
+}
+
+func newMergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	} else if list1.Val <= list2.Val {
+		list1.Next = newMergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = newMergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
