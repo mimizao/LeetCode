eb549104d4bde904da612295b0fedef785d747ff
mimizao <88256191@qq.com>
2022-07-26 20:25:52 +0800

    test

---
+++ 26.删除有序数组中的重复项.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除有序数组中的重复项
+ */
+package main
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	len := len(nums)
+	return len
+}
+
+// @lc code=end
