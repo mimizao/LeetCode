1277981968f2adcc157b74ac27983b6e04412ac7
孙浩天 <88256191@qq.com>
2022-07-27 10:54:01 +0800

    RemoveDuplicates own idea c#

---
+++ 26.删除有序数组中的重复项.cs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=csharp
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int len = nums.Length;
+        if (len <= 1)
+        {
+            return len;
+        }
+        int res = len;
+        int left = 0;
+        int right = 1;
+        while (right < len)
+        {
+            if (nums[left] >= nums[right])
+            {
+                if (nums[right] == nums[len - 1])
+                {
+                    System.Console.WriteLine(right);
+                    return right;
+                }
+                int index = right;
+                while (index < len - 1)
+                {
+                    if (nums[index] > nums[left] && nums[index] > nums[right])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+                nums[right] = nums[index];
+                res--;
+            }
+            left++;
+            right++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
