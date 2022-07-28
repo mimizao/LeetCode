b980b5955e0ed621bb3cd229b42b67f51215f406
mimizao <88256191@qq.com>
2022-07-28 13:18:11 +0800

    remove_element own idea rust

---
+++ 27.移除元素.rs
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=rust
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let len = nums.len();
+        if len == 0 {
+            return len as i32;
+        }
+        nums.sort_unstable();
+        let mut count = 0;
+        for i in 0..len {
+            if nums[i] == val {
+                count += 1;
+                if nums[i] == nums[len - count] {
+                    return i as i32;
+                }
+                if i < len - count {
+                    nums[i] = nums[len - count];
+                } else {
+                    return (len - count) as i32;
+                }
+            } else if nums[i] > val {
+                break;
+            }
+        }
+        (len - count) as i32
+    }
+}
+// @lc code=end
