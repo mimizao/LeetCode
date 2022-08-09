790c32cc85d50e7382a79477ee52175e076d82ee
mimizao <88256191@qq.com>
2022-08-09 13:20:17 +0800

    38.外观数列&39.组合总和 test c#

---
+++ LocalTest/C#/38.外观数列/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        string res = "1";
+        for (int i = 2; i <= n; i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0, start = 0; j < res.Length; start = j)
+            {
+                while (j < res.Length && res[j] == res[start])
+                {
+                    j++;
+                }
+                sb.Append(j - start).Append(res[start]);
+            }
+            res = sb.ToString();
+        }
+        return res;
+    }
+}
---
+++ LocalTest/C#/39.组合总和/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] candidates = new int[] { 2, 3, 6,7 };
+int target = 7;
+var res = Solution.CombinationSum(candidates, target);
+Console.WriteLine(res);
+
+public class Solution
+{
+    public static IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        int len = candidates.Length;
+        IList<IList<int>> res = new List<IList<int>>();
+        if (len == 0)
+        {
+            return res;
+        }
+        Stack<int> path = new Stack<int>();
+        dfs(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    public static void dfs(int[] candidates, int begin, int len, int target, Stack<int> path, IList<IList<int>> res)
+    {
+        if (target < 0)
+        {
+            return;
+        }
+        if (target == 0)
+        {
+            res.Add(new List<int>(path));
+            return;
+        }
+        for (int i = begin; i < len; i++)
+        {
+            path.Push(candidates[i]);
+            dfs(candidates, i, len, target - candidates[i], path, res);
+            path.Pop();
+        }
+    }
+}
