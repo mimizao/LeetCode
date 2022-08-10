86329e8eaaa8866aeaee63973cf2a87c64c0159a
mimizao <88256191@qq.com>
2022-08-10 16:44:30 +0800

    combinationSum own idea go

--- 39.组合总和.go
+++ 39.组合总和.go
@@ -7,25 +7,28 @@ package main
 
 // @lc code=start
 func combinationSum(candidates []int, target int) [][]int {
+	n := len(candidates)
 	res := [][]int{}
-	comb := []int{}
-	var dfs func(target, index int)
-	dfs = func(target, index int) {
-		if index == len(candidates) {
-			return
-		}
-		if target == 0 {
-			res = append(res, append([]int(nil), comb...))
-			return
-		}
-		dfs(target, index+1)
-		if target-candidates[index] >= 0 {
-			comb = append(comb, candidates[index])
-			dfs(target-candidates[index], index)
-			comb = comb[:len(comb)-1]
-		}
+	if n == 0 {
+		return res
 	}
-	dfs(target, 0)
+	path := []int{}
+	res = dfs(candidates, 0, n, target, path, res)
+	return res
+}
+
+func dfs(candidates []int, begin, n, target int, path []int, res [][]int) [][]int {
+	if target < 0 {
+		return res
+	}
+	if target == 0 {
+		res = append(res, append([]int(nil), path...))
+	}
+	for i := begin; i < n; i++ {
+		path = append(path, candidates[i])
+		res = dfs(candidates, i, n, target-candidates[i], path, res)
+		path = path[:len(path)-1]
+	}
 	return res
 }
 
