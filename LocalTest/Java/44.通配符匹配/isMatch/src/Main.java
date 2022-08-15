f1beac6e5d88fd58a1c9d9ad6418aa920db8b838
mimizao <88256191@qq.com>
2022-08-15 16:37:59 +0800

    44.通配符匹配 java test

---
+++ LocalTest/Java/44.通配符匹配/isMatch/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int lenS = s.length();
+        int lenP = p.length();
+        boolean[][] dp = new boolean[lenS + 1][lenP + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= lenP; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i <= lenS; i++) {
+            for (int j = 1; j <= lenP; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[lenS][lenP];
+    }
+}
