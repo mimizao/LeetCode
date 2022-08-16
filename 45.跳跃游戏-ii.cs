3d6f00e95ffac21e02ce5c081bfd71afd117f949
mimizao <88256191@qq.com>
2022-08-16 12:46:47 +0800

    Jump own idea c#

--- 45.跳跃游戏-ii.cs
+++ 45.跳跃游戏-ii.cs
@@ -5,26 +5,38 @@
  */
 
 // @lc code=start
-public class Solution {
-    public int Jump(int[] nums) {
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
         int len = nums.Length;
         int count = 0;
         int begin = 0;
-        while(begin <= len-1) {
-            if (begin == len-1){
+        while (begin <= len - 1)
+        {
+            if (begin == len - 1)
+            {
                 break;
-            }else if (begin + nums[begin]>=len-1){
+            }
+            else if (begin + nums[begin] >= len - 1)
+            {
                 count++;
                 break;
-            }else{
+            }
+            else
+            {
                 int temp = 0;
                 int newBegin = begin;
-                for (int i = 1; i<=nums[begin];i++){
-                    if (temp<=nums[begin+i]+i){
-                        temp = nums[begin+i]+i;
+                for (int i = 1; i <= nums[begin]; i++)
+                {
+                    if (temp <= nums[begin + i] + i)
+                    {
+                        temp = nums[begin + i] + i;
                         newBegin = begin + i;
                     }
                 }
+                count++;
+                begin = newBegin;
             }
         }
         return count;
