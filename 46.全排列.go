6740a74aa4a9b51196793c11f9e70559a6f92327
mimizao <88256191@qq.com>
2022-08-17 19:15:05 +0800

    permute offical idea go

--- 46.全排列.go
+++ 46.全排列.go
@@ -3,10 +3,35 @@
  *
  * [46] 全排列
  */
+package main
 
 // @lc code=start
 func permute(nums []int) [][]int {
-
+	var res [][]int
+	if len(nums) == 0 {
+		return res
+	}
+	var path []int
+	used := make([]bool, len(nums))
+	var dfs func()
+	dfs = func() {
+		if len(path) == len(nums) {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
+				continue
+			}
+			path = append(path, nums[i])
+			used[i] = true
+			dfs()
+			used[i] = false
+			path = path[0 : len(path)-1]
+		}
+	}
+	dfs()
+	return res
 }
-// @lc code=end
 
+// @lc code=end
