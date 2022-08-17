1fa6da2663a9bbaacd0467b3754f678a6b54e96c
mimizao <88256191@qq.com>
2022-08-17 19:16:47 +0800

    permute offical idea java

--- 46.全排列.java
+++ 46.全排列.java
@@ -7,7 +7,32 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(nums, used, path, res);
+        return res;
+    }
 
+    public void dfs(int[] nums, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            path.push(nums[i]);
+            used[i] = true;
+            dfs(nums, used, path, res);
+            used[i] = false;
+            path.pop();
+        }
     }
 }
 // @lc code=end
