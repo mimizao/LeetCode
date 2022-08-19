25adcf85ebc346993cdcaaeea5107fcffea46621
mimizao <88256191@qq.com>
2022-08-19 08:53:26 +0800

    48.旋转图像 test

---
+++ LocalTest/C#/48.旋转图像/Program.cs
@@ -0,0 +1,40 @@
+﻿int[][] matrix = new[]
+    { new[] { 5, 1, 9, 11 }, new[] { 2, 4, 8, 10 }, new[] { 13, 3, 6, 7 }, new[] { 15, 14, 12, 16 } };
+Solution solution = new Solution();
+solution.Rotate(matrix);
+for (int i = 0; i < matrix.Length; i++)
+{
+    for (int j = 0; j < matrix[0].Length; j++)
+    {
+        Console.Write(matrix[i][j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+public class Solution
+{
+    public void Rotate(int[][] matrix)
+    {
+        int len = matrix.Length;
+        if (len == 1) return;
+        int temp1, temp2, temp3, temp4;
+        for (int i = 0; i < (len + 1) / 2; i++)
+        {
+            for (int j = i; j < len - i - 1; j++)
+            {
+                temp1 = matrix[i][j];
+                temp2 = matrix[j][len - i - 1];
+                temp3 = matrix[len - i - 1][len - j - 1];
+                temp4 = matrix[len - j - 1][i];
+
+                matrix[i][j] = temp4;
+                matrix[j][len - i - 1] = temp1;
+                matrix[len - i - 1][len - j - 1] = temp2;
+                matrix[len - j - 1][i] = temp3;
+            }
+        }
+
+        return;
+    }
+}
---
+++ LocalTest/Java/48.旋转图像/rotate/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        if (len == 1) {
+            return;
+        }
+        int temp1, temp2, temp3, temp4;
+        for (int i = 0; i < (len + 1) / 2; i++) {
+            for (int j = i; j < len - i - 1; j++) {
+                temp1 = matrix[i][j];
+                temp2 = matrix[j][len - i - 1];
+                temp3 = matrix[len - i - 1][len - j - 1];
+                temp4 = matrix[len - j - 1][i];
+
+                matrix[i][j] = temp4;
+                matrix[j][len - i - 1] = temp1;
+                matrix[len - i - 1][len - j - 1] = temp2;
+                matrix[len - j - 1][i] = temp3;
+            }
+        }
+        return;
+    }
+}
