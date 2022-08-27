f6d4e0bcc495d337ba15d0c78adb170b58f412dc
mimizao <88256191@qq.com>
2022-08-27 14:48:23 +0800

    merge own idea java

---
+++ 56.合并区间.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[0] - interval2[0];
+            }
+        });
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int L = interval[0], R = interval[1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
+// @lc code=end
+
