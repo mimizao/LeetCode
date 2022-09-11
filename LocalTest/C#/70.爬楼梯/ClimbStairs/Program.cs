f4445ec1d3931d4ea04692f19c0cffe424e02399
mimizao <88256191@qq.com>
2022-09-11 09:00:28 +0800

    70.爬楼梯

---
+++ LocalTest/C#/70.爬楼梯/ClimbStairs/Program.cs
@@ -0,0 +1,17 @@
+﻿var solution = new Solution();
+const int n = 45;
+var res = solution.ClimbStairs(n);
+Console.WriteLine(res);
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n is 1 or 2) return n;
+        var dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (var i = 2; i < n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n - 1];
+    }
+}
