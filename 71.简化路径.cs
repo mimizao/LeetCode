e3df4d8d7da505b8955a0a780bb2b16a6379bbd0
mimizao <88256191@qq.com>
2022-09-12 09:25:11 +0800

    SimplifyPath own idea c#

---
+++ 71.简化路径.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=csharp
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+public class Solution
+{
+    public string SimplifyPath(string path)
+    {
+        string[] strs = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        Stack<string> resStack = new Stack<string>();
+        resStack.Push("/");
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i] == "" || strs[i] == "." || (resStack.Count == 1 && strs[i] == ".."))
+            {
+                continue;
+            }
+            if (strs[i] == ".." && resStack.Count > 1)
+            {
+                resStack.Pop();
+            }
+            else
+            {
+                if (i == strs.Length - 1)
+                {
+                    resStack.Push(strs[i]);
+                }
+                else
+                {
+                    resStack.Push((strs[i] + "/"));
+                }
+            }
+        }
+        while (resStack.Count > 1 && resStack.Peek() == "/")
+        {
+            resStack.Pop();
+        }
+        string res = string.Empty;
+        while (resStack.Count > 0)
+        {
+            res = resStack.Pop() + res;
+        }
+        while (res[^1] == '/' && res.Length > 1)
+        {
+            res = res[..^1];
+        }
+        return res;
+    }
+}
+// @lc code=end
+
