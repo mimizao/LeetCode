e8d07ba594c4fec915147d7419d564a7dddfc647
mimizao <88256191@qq.com>
2022-09-18 19:24:09 +0800

    Combine own idea

---
+++ 77.组合.cs
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=csharp
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+public class Solution
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        IList<IList<int>> res = new List<IList<int>>();
+        Stack<int> path = new Stack<int>();
+        bool[] used = new bool[n + 1];
+        Dfs(res, path, used, n, k);
+        return res;
+    }
+
+    private void Dfs(IList<IList<int>> res, Stack<int> path, bool[] used, int n, int k)
+    {
+        if (path.Count == k)
+        {
+            res.Add(new List<int>(path));
+            return;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (used[i] || (path.Count > 0 && i > path.Peek()))
+            {
+                continue;
+            }
+            used[i] = true;
+            path.Push(i);
+            Dfs(res, path, used, n, k);
+            path.Pop();
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
