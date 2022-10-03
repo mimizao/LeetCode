ae85a27188589e41708312d00374fb10c1970e58
mimizao <88256191@qq.com>
2022-10-03 19:00:11 +0800

    91.解码方法 c++ test

---
+++ LocalTest/C++/91.解码方法/numDecodings/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0') {
+            return 0;
+        }
+        auto len = s.size();
+        if (len == 1) {
+            return s == "0" ? 0 : 1;
+        }
+        if (len == 2) {
+            int temp = stoi(s);
+            if (temp % 10 == 0 && temp >= 30) {
+                return 0;
+            } else if (temp > 26 || temp == 10 || temp == 20) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+        vector<int> dp(len + 1, 0);
+        dp[1] = numDecodings(s.substr(len - 1));
+        dp[2] = numDecodings(s.substr(len - 2));
+        for (auto i = 3; i <= len; ++i) {
+            string tempStr = s.substr(len - i, 2);
+            int tempNum = numDecodings(tempStr);
+            if (tempNum == 0) {
+                dp[i] = 0;
+            } else if (tempNum == 1) {
+                if (tempStr == "10" || tempStr == "20") {
+                    dp[i] = dp[i - 2];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[len];
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "3123";
+    auto res = solution.numDecodings(s);
+    std::cout << res << std::endl;
+    return 0;
+}
