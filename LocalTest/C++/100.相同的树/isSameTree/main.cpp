bd95d186eb98489024cadf988508bb4716fad0cb
mimizao <88256191@qq.com>
2022-10-19 10:48:50 +0800

    100.相同的树 c++ test

---
+++ LocalTest/C++/100.相同的树/isSameTree/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if ((p == nullptr && q != nullptr) || (p != nullptr && q == nullptr) || (p->val != q->val)) {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
